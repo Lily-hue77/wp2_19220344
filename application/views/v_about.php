5c9f0bd61db4092c0211aa85a2df4d537bd143d3
Lily-hue77 <19220344@bsi.ac.id>
2023-10-09 20:37:28 +0700

    bikin home dan about web rental buku

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Yuli</li>
+        <li>Nama Belakang : ani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Delima no.58 Rt 07/01 Srengseng, Kembangan, Jakarta Barat.</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Banyumas</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section
